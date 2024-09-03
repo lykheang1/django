3e4bfe2993f12f1fc231fcab79d74775a998cd8b
MOEURN-Lykheang <ly.kheang.jr@gmail.com>
2024-09-03 16:35:51 +0700

    dj syntax

--- members/views.py
+++ members/views.py
@@ -24,3 +24,12 @@ def detail(request, id):
 def main(request):
     template = loader.get_template('main.html')
     return HttpResponse(template.render())
+
+def testing(request): 
+    mymembers = Member.objects.all().values()
+    template = loader.get_template('template.html')
+    context = {
+        "mymembers" : mymembers
+    }
+    
+    return HttpResponse(template.render(context, request))
